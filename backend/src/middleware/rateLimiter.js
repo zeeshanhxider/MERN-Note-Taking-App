1910f6968a42ea2fd284be5de132c3717e4d76aa
Zeeshan Haider <zeeshanhxider@gmail.com>
2025-08-02 04:49:29 +0500

    fixed rate limiting issue

--- backend/src/middleware/rateLimiter.js
+++ backend/src/middleware/rateLimiter.js
@@ -1,17 +1,29 @@
-import ratelimit from '../config/upstash.js';
+import ratelimit from "../config/upstash.js";
 
 const rateLimiter = async (req, res, next) => {
-    try {
-        const { success } = await ratelimit.limit("rate-limit-key");
-        if (!success) {
-            return res.status(429).json({ message: 'Too many requests, please try again later.' });
-        }
-        next();
+  if (req.path === "/favicon.png") return next();
+  try {
+    if (
+      req.path.startsWith("/assets") ||
+      req.path === "/favicon.png" ||
+      req.path.endsWith(".js") ||
+      req.path.endsWith(".css") ||
+      req.path.endsWith(".png") ||
+      req.path.endsWith(".ico")
+    ) {
+      return next();
     }
-    catch (error) {
-        console.log("Rate limiter error:", error);
-        next(error);
+    const { success } = await ratelimit.limit("rate-limit-key");
+    if (!success) {
+      return res
+        .status(429)
+        .json({ message: "Too many requests, please try again later." });
     }
-}
+    next();
+  } catch (error) {
+    console.log("Rate limiter error:", error);
+    next(error);
+  }
+};
 
 export default rateLimiter;
